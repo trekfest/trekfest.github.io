6f48b409f1a866f8ef475655757979898895c527
trekfest <hrynyshyn-ee27@lll.kpi.ua>
2024-06-25 10:28:31 +0000

    Add new files: description of changes

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
 
-app_name = 'polls'  # This is the application namespace
+app_name = 'polls'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # Add more paths for other views if necessary
+    path('owner', views.owner, name='owner'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
